194f47b9fdb57e93cab167ef5728c33f4c2531cb
ClownTripleSrank <145134985+ChaikaMe@users.noreply.github.com>
2023-12-19 15:25:11 +0200

    last-fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,7 @@
 function getElementWidth(content, padding, border) {
-    console.log(Number.parseFloat(content)+Number.parseFloat(padding)*2+Number.parseFloat(border)*2)
+    return(Number.parseFloat(content)+Number.parseFloat(padding)*2+Number.parseFloat(border)*2)
 }
 
-getElementWidth("50px", "8px", "4px"); // 74
-getElementWidth("60px", "12px", "8.5px"); // 101
-getElementWidth("200px", "0px", "0px"); // 200
-
-// Не бачу сенсу дублювати console.log для виклику функції, викликає купу undefined. Якщо це не правильно прошу поясніть чому. 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
